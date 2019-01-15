0606d50c9869ad56ac4bcd415170332bb4b99be8
AN ZHU <zhua@kean.edu>
2019-01-15 12:41:16 -0700

    Time Bar

---
+++ Assets/Script/CountDownTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDownTimer : MonoBehaviour
+{
+    public ProBar bar;
+    public float TimeinSeconds;
+    private float currentTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = TimeinSeconds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+        bar.SetProgress(currentTime / TimeinSeconds);
+    }
+}
