253402c91714cc63676a5230e626e27e4a2dc63d
AN ZHU <zhua@kean.edu>
2019-01-15 11:24:34 -0700

    Commit

---
+++ Assets/Script/ProBar.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject BackGround;
+    public GameObject BAR;
+    public float SCalex;
+    public float currentratio;
+    void Start()
+    {
+        SCalex = BackGround.GetComponent<Transform>().localScale.x;
+        currentratio = 1.0f;
+    }
+    public void SetProgress(float ratio) {
+        ratio = Mathf.Clamp(ratio, 0.0f, 1.0f);
+        this.currentratio = ratio;
+        float oldsize = BAR.GetComponent<Transform>().localScale.x;
+        float currentsize = ratio * SCalex;
+        float deltaSize = currentsize - oldsize;
+        float y = BAR.GetComponent<Transform>().localScale.y;
+        float z = BAR.GetComponent<Transform>().localScale.z;
+        BAR.GetComponent<Transform>().localScale = new Vector3(currentsize, y, z);
+        BAR.GetComponent<Transform>().Translate(new Vector3(-deltaSize*5.0F, 0f,0f));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.S)) {
+            SetProgress(currentratio + 0.01f);
+            Debug.Log("KEY S Pressed");
+        }
+        else if(Input.GetKey(KeyCode.W)&&Input.GetKey(KeyCode.A)){
+            SetProgress(currentratio - 0.01f);
+            Debug.Log("KEY A and W Pressed");
+        }
+    }
+}
